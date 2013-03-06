fee68ceb309b131d0d2e238f04e9e5c86ebf78c7
gtracy <gtracy@gmail.com>
2013-03-05 18:53:56 -0600

    ported interface to use XMPP

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import Cookie
 
 from google.appengine.api import memcache
+from google.appengine.api import xmpp
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -47,17 +48,101 @@ class TestHandler(webapp.RequestHandler):
         self.response.headers.add_header("Set-Cookie", createCookie('counter',cookie_counter))
         self.response.out.write(question)
         
-class MainHandler(webapp.RequestHandler):
+class XmppHandler(webapp.RequestHandler):
     def get(self):
         self.post()
         
     def post(self):
+      message = xmpp.Message(self.request.POST)
+      total_questions = 10
+      
+      # who called? and what did they say?
+      if message.sender.find('@'):
+          phone = message.sender.split('/')[0]
+      else:
+          phone = message.sender.get('from')
+      msg = message.body.lower()
+
+      # create a user instance if we need to
+      create_user(phone)
+      
+      #self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+      
+      # take a look at the request and see if it is valid
+      # if it is, process the request
+      
+      if msg.lower() == 'start':
+          
+          memcache.set(phone,0)
+          
+          num1 = random.randint(0,10)
+          num2 = random.randint(0,10)
+          cookie_counter = 1
+          cookie_question = '%s-%s-m-%s' % (num1,num2,cookie_counter)
+          question = "welcome! i'll send %s questions. just reply with your answer. here's the first - %s x %s" % (str(total_questions),str(num1), str(num2))
+          
+          logging.debug('cookie_question %s' % cookie_question)
+          memcache.set('question',cookie_question)
+          message.reply(question)
+
+      elif msg.isdigit():
+          
+          # validate answer message
+          cookie_question = memcache.get('question')
+          if cookie_question is not None:
+             
+              # current state is stored in cookies
+              cookie_counter = int(cookie_question.split('-')[3])
+
+              # check answer
+              answer = computeAnswer(cookie_question, int(msg))
+              if answer == int(msg):
+                  correct = True
+                  memcache.incr(phone)
+              else:
+                  correct = False
+
+              encourage = pickFeedback(correct, answer)     
+              
+              # store the problem in the datastore
+              createLog(phone,cookie_question,msg,correct)
+              
+              # are we done yet?
+              if cookie_counter == total_questions:
+                  # compute results
+                  message.reply('%s all done! you got %s out of %s correct.' % (encourage, memcache.get(phone),total_questions))
+              else:
+                  # provide feedback and move to the next problem
+                  cookie_counter += 1
+                  cookie_question = createProblem(cookie_counter)
+                  question = createQuestionString(cookie_question)
+                  memcache.set('question',cookie_question)
+                  message.reply(encourage + question)
+                                    
+          else:
+              logging.error('missing cookie')
+              logging.debug(self.request.cookies)
+              message.reply('something broke inside the app! i need to stop. :(')
+              
+      else:
+        response = 'Yah. Nice try. I need numbers dude'
+        message.reply(response)
+        
+      return
+      
+
+## end XmppHandler
+
+class MainHandler(webapp.RequestHandler):
+        
+    def post(self):
+
       total_questions = 10
       
       # who called? and what did they say?
       phone = self.request.get("From")
       msg = self.request.get("Body")
-      
+
       # create a user instance if we need to
       create_user(phone)
       
@@ -125,20 +210,19 @@ class MainHandler(webapp.RequestHandler):
         
       return
       
-
-## end MainHandler
+## end XmppHandler
 
 def createProblem(counter):
     
     if random.randint(0,10) < 5:
         # create multiplication problem
-        num1 = random.randint(0,10)
-        num2 = random.randint(0,10)
+        num1 = random.randint(5,9)
+        num2 = random.randint(4,10)
         return '%s-%s-m-%s' % (num1,num2,counter)
     else:
         # create division problem
-        num1 = random.randint(2,9)
-        num2 = random.randint(2,10)
+        num1 = random.randint(5,9)
+        num2 = random.randint(5,10)
         return '%s-%s-d-%s' % (num1*num2,num1,counter)
         
 # end
@@ -202,6 +286,7 @@ def createCookie(cookieName, cookieVal):
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
     application = webapp.WSGIApplication([('/sms', MainHandler),
+                                          ('/_ah/xmpp/message/chat/', XmppHandler),
                                           ('/test', TestHandler)
                                          ],
                                          debug=True)
